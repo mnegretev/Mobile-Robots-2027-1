c9b93873e41ebd7a0a6113dc46d633f421fb86b0
juandiego560 <juandiego200139@gmail.com>
2026-08-27 01:40:08 -0600

    funciono_bien

--- ros2_ws/src/navigation/navig_utils/navig_utils/ros_basics.py
+++ ros2_ws/src/navigation/navig_utils/navig_utils/ros_basics.py
@@ -13,7 +13,7 @@ from rclpy.duration import Duration
 from geometry_msgs.msg import Twist, PointStamped
 from sensor_msgs.msg import LaserScan
 
-FULL_NAME = "FULL NAME"
+FULL_NAME = "Juan Diego Bello Jose"
 
 SM_INIT = 0
 SM_FORWARD = 10
@@ -54,7 +54,18 @@ class RosBasicsNode(Node):
             # If there is an obstacle on the right, then turn left
             # If there is an obstacle in front, then turn around
             #
-
+            if self.obstacle_left:
+                self.get_logger().info("Obstacle on the left")
+                self.move(0, -0.78, 2.0)
+            elif self.obstacle_right:
+                self.get_logger().info("Obstacle on the right")
+                self.move(0, 0.78, 2.0)
+            elif self.obstacle_front:
+                self.get_logger().info("Obstacle in front")
+                self.move(0, 0.78, 4.0)
+            else:
+                self.get_logger().info("Moving forward")
+                self.move(0.5, 0.0, 0.5)
             #
             # END OF TODO
             #
@@ -70,6 +81,10 @@ class RosBasicsNode(Node):
         # with True or False, accordingly.
         # Check online documentation of LaserScan message
         #
+        n = len(msg.ranges)
+        self.obstacle_left = msg.ranges[n//2 + 100] < 2.0
+        self.obstacle_right = msg.ranges[n//2 - 100] < 2.0
+        self.obstacle_front = msg.ranges[n//2] < 2.0
         
         return
 
