317a7e8de2882099c7ca3ebfcdc9fc3ae6723635
Renata <reno5539@gmail.com>
2026-09-10 00:34:58 -0600

    Imprementación Path Smoothing

--- ros2_ws/src/navigation/path_planner/path_planner/path_smoothing.py
+++ ros2_ws/src/navigation/path_planner/path_planner/path_smoothing.py
@@ -13,7 +13,7 @@ from geometry_msgs.msg import Pose, PoseStamped, Point
 from navig_msgs.srv import ProcessPath
 import numpy
 
-NAME = "FULL NAME"
+NAME = "Baños Reyes Renata"
 
 class PathSmoothingNode(Node):
     def smooth_path(self, Q, w1, w2, max_steps):
@@ -30,7 +30,14 @@ class PathSmoothingNode(Node):
         # The smoothed path must have the same shape.
         # Return the smoothed path.
         #
-
+        step = 0
+        nabla[0] = 0
+        nabla[-1]= 0
+        while step < max_steps and numpy.linalg.norm(nabla) > tol:
+            for i in range (1, len(Q) -1):
+                nabla[i] = w1*(2*P[i] - P[i-1] - P[i+1]) + w2*(P[i] - Q[i])
+            P = P - epsilon * nabla
+            step += 1
         #
         # END OF TODO
         #
